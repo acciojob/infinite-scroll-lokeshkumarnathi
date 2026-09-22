83c59ae180a9203a18fe12f9fe1fa02a3d63b09d
LOKESH KUMAR NATHI <147913462+lokeshkumarnathi@users.noreply.github.com>
2026-09-22 17:19:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
 //your code here!
+const list = document.getElementById("list");
 
+let count = 1;
+
+
+// Add a list item
+function addItem() {
+  const li = document.createElement("li");
+
+  li.textContent = "List Item " + count;
+
+  list.appendChild(li);
+
+  count++;
+}
+
+
+// Add 10 items by default
+for (let i = 0; i < 10; i++) {
+  addItem();
+}
+
+
+// Add 2 more items when user reaches the bottom
+window.addEventListener("scroll", function () {
+
+  const scrollPosition =
+    window.innerHeight + window.scrollY;
+
+  const pageHeight =
+    document.documentElement.scrollHeight;
+
+  if (scrollPosition >= pageHeight - 5) {
+
+    addItem();
+    addItem();
+
+  }
+
+});
